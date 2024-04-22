07def54578f492f191ad83724aafa88a7ec6b4ec
HxcoderIDOfc <149635285+HxcoderIDOfc@users.noreply.github.com>
2024-04-22 17:04:43 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  const data = [
+    "0",
+  ];
+
+  res.json(data);
+});
+
+app.listen(3000, () => {
+  console.log("Server berjalan pada port 3000");
+});
